2af2360630b9a503e97705550ed5df0b33dc2e2b
Matthieu Vanin <matthieu.vanin@freescale.com>
2014-03-11 11:26:23 +0100

    arm: imx: Add few missing macros for VF610 in IOMUX_PAD structure
    Signed-off-by: Irina Presa <Irina.Presa@freescale.com>

--- arch/arm/include/asm/imx-common/iomux-v3.h
+++ arch/arm/include/asm/imx-common/iomux-v3.h
@@ -120,11 +120,14 @@ typedef u64 iomux_v3_cfg_t;
 #define PAD_CTL_SRE			(1 << 11)
 
 #define PAD_CTL_DSE_50ohm	(3 << 6)
+#define PAD_CTL_DSE_37ohm	(4 << 6)
+#define PAD_CTL_DSE_30ohm	(5 << 6)
 #define PAD_CTL_DSE_25ohm	(6 << 6)
 #define PAD_CTL_DSE_20ohm	(7 << 6)
 
 #define PAD_CTL_PUS_47K_UP	(1 << 4 | PAD_CTL_PUE)
 #define PAD_CTL_PUS_100K_UP	(2 << 4 | PAD_CTL_PUE)
+#define PAD_CTL_PUS_22K_UP	(3 << 4 | PAD_CTL_PUE)
 #define PAD_CTL_PKE		(1 << 3)
 #define PAD_CTL_PUE		(1 << 2 | PAD_CTL_PKE)
 
