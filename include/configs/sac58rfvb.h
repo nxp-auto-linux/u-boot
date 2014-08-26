d7496711f0648c12d39a631db01681e4d330a11a
Gilles Talis <gilles.talis@freescale.com>
2014-08-26 10:37:14 +0200

    sac58r FVB config file: set DDR boot variables and select SDHC2 as boot device
    Signed-off-by: Gilles Talis <gilles.talis@freescale.com>

--- include/configs/sac58rfvb.h
+++ include/configs/sac58rfvb.h
@@ -24,9 +24,11 @@
 
 #include <asm/arch/imx-regs.h>
 #include <config_cmd_default.h>
+#include <linux/sizes.h>
+
 
 #define CONFIG_SAC58R
-#define CONFIG_RUN_FROM_IRAM_ONLY
+//#define CONFIG_RUN_FROM_IRAM_ONLY
 
 #define CONFIG_DISPLAY_CPUINFO
 #define CONFIG_DISPLAY_BOARDINFO
@@ -42,7 +44,7 @@
 #endif
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		(4 * 1024)
+#define CONFIG_SYS_MALLOC_LEN		(1 * SZ_1M)
 
 #define CONFIG_BOARD_EARLY_INIT_F
 
@@ -61,14 +63,16 @@
 #define CONFIG_FSL_USDHC
 #define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC0_BASE_ADDR
 #define CONFIG_SYS_FSL_ESDHC_NUM	3
+#define CONFIG_SYS_MMC_ENV_DEV		2
+
 
 #define CONFIG_SYS_FSL_ERRATUM_ESDHC111
 
 #define CONFIG_CMD_MMC
 #define CONFIG_GENERIC_MMC
-//#define CONFIG_CMD_EXT2		/* EXT2 Support			*/
-//#define CONFIG_CMD_FAT		/* FAT support			*/
-//#define CONFIG_DOS_PARTITION
+#define CONFIG_CMD_EXT2		/* EXT2 Support			*/
+#define CONFIG_CMD_FAT		/* FAT support			*/
+#define CONFIG_DOS_PARTITION
 
 #if 0
 /* Ethernet config */
@@ -114,20 +118,19 @@
 
 #define CONFIG_BOOTDELAY		3
 
-#define CONFIG_LOADADDR			0x82000000
+#define CONFIG_LOADADDR				0x82000000
 #ifdef CONFIG_RUN_FROM_IRAM_ONLY
 #define CONFIG_SYS_MALLOC_BASE		0x3f004000 // HACK
 #endif
-#define CONFIG_SYS_TEXT_BASE		0x3E808000
+#define CONFIG_SYS_TEXT_BASE		0x87800000
 
-#if 0 /* MATT: no need to have env settings now, will enable later when booting Linux */
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"script=boot.scr\0" \
 	"uimage=uImage\0" \
-	"console=ttyLP1\0" \
+	"console=ttyLP0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=vf610-twr.dtb\0" \
+	"fdt_file=sac58r-fvb.dtb\0" \
 	"fdt_addr=0x81000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
@@ -208,13 +211,10 @@
 		   "fi; " \
 	   "else run netboot; fi"
 
-#endif //#if 0
 
 /* Miscellaneous configurable options */
-#if 0 /* no need to have env settings now, will enable later when booting Linux */
 #define CONFIG_SYS_LONGHELP		/* undef to save memory */
 #define CONFIG_SYS_HUSH_PARSER		/* use "hush" command parser */
-#endif //#if 0
 #define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
 #define CONFIG_SYS_PROMPT		"=> "
 #undef CONFIG_AUTO_COMPLETE
@@ -230,18 +230,18 @@
 #define CONFIG_SYS_MEMTEST_END		0x87C00000
 
 #define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
-#define CONFIG_SYS_HZ			1000
+#define CONFIG_SYS_HZ				1000
 
 /*
  * Stack sizes
  * The stack sizes are set up in start.S using the settings below
  */
-#define CONFIG_STACKSIZE		(16 * 1024)	/* regular stack */
+#define CONFIG_STACKSIZE			(128 * 1024)	/* regular stack */
 
 /* Physical memory map */
 #define CONFIG_NR_DRAM_BANKS		1
-#define PHYS_SDRAM			(0x80000000)
-#define PHYS_SDRAM_SIZE			(128 * 1024 * 1024)
+#define PHYS_SDRAM					(0x80000000)
+#define PHYS_SDRAM_SIZE				(256 * 1024 * 1024)
 
 #define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
 #define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
@@ -263,7 +263,6 @@
 //#endif
 
 #define CONFIG_ENV_OFFSET		(12 * 64 * 1024)
-#define CONFIG_SYS_MMC_ENV_DEV		0
 
 #define CONFIG_OF_LIBFDT
 #define CONFIG_CMD_BOOTZ
