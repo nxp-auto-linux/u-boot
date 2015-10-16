50aa90532a7d46dd9af11fe61f8de60662d076dc
Grigore Lupescu <grigore.lupescu@freescale.com>
2015-10-16 15:12:44 +0300

    ALB-112 Fix DCU buffer underrun problems
    Signed-off-by: Grigore Lupescu <grigore.lupescu@freescale.com>

--- board/freescale/s32v234evb/s32v234evb.c
+++ board/freescale/s32v234evb/s32v234evb.c
@@ -246,6 +246,40 @@ int board_phy_config(struct phy_device *phydev)
 	return 0;
 }
 
+#ifdef CONFIG_DCU_QOS_FIX
+int board_dcu_qos()
+{
+	writel(0x0, 0x40012380);
+	writel(0x0, 0x40012384);
+	writel(0x0, 0x40012480);
+	writel(0x0, 0x40012484);
+	writel(0x0, 0x40012580);
+	writel(0x0, 0x40012584);
+	writel(0x0, 0x40012680);
+	writel(0x0, 0x40012684);
+	writel(0x0, 0x40012780);
+	writel(0x0, 0x40012784);
+	writel(0x0, 0x40012880);
+	writel(0x0, 0x40012884);
+	writel(0x0, 0x40012980);
+	writel(0x0, 0x40012984);
+	writel(0x0, 0x40012A80);
+	writel(0x0, 0x40012A84);
+	writel(0x0, 0x40012B80);
+	writel(0x0, 0x40012B84);
+	writel(0x0, 0x40012C80);
+	writel(0x0, 0x40012C84);
+	writel(0x0, 0x40012D80);
+	writel(0x0, 0x40012D84);
+	writel(0x0, 0x40012E80);
+	writel(0x0, 0x40012E84);
+	writel(0x0, 0x40012F80);
+	writel(0x0, 0x40012F84);
+	
+	return 0;
+}
+#endif
+
 int board_early_init_f(void)
 {
 	clock_init();
@@ -257,6 +291,11 @@ int board_early_init_f(void)
 #ifdef CONFIG_SYS_USE_NAND
 	setup_iomux_nfc();
 #endif
+
+#ifdef CONFIG_DCU_QOS_FIX
+	board_dcu_qos();
+#endif
+
 	return 0;
 }
 
