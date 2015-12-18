a6090e3eba6a852112e720fd740dc287a6f33306
Ana Nedelcu <ana-florentina.nedelcu@nxp.com>
2015-12-18 21:41:41 +0200

    ALB172: add common configuration header in u-boot for s32v234 boards
    Added in s32v234_common.h file just the common part of the boards' configuration
    header. Made code refactoring to eliminate duplicates.
    
    Added SMP defines in the common header for fvb and fvb_revb images to be able
    to build.
    
    Signed-off-by: Ana Nedelcu <ana-florentina.nedelcu@nxp.com>

--- include/configs/s32v234fvb_revb.h
+++ include/configs/s32v234fvb_revb.h
@@ -22,20 +22,11 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#include <asm/arch/imx-regs.h>
-#include <config_cmd_default.h>
-
 #define CONFIG_S32V234
 #define CONFIG_S32VFVB_REVB
 #define CONFIG_DISPLAY_CPUINFO
 #define CONFIG_DISPLAY_BOARDINFO
 
-/* Config GIC */
-#define CONFIG_GICV2
-#define GICD_BASE 0x7D001000
-#define GICC_BASE 0x7D002000
-
-#define CONFIG_REMAKE_ELF
 #define CONFIG_RUN_FROM_IRAM_ONLY
 
 #define CONFIG_MACH_TYPE		4146
@@ -53,106 +44,28 @@
 /* S32V234 FVB Rev. B has DDR3 */
 #define CONFIG_S32V234_DDR2
 
-#ifdef CONFIG_RUN_FROM_DDR0
-#define DDR_BASE_ADDR	0x80000000
-#define DDR_SIZE		(1024 * 1024 * 1024)
-#else
-#define DDR_BASE_ADDR	0xC0000000
-#define DDR_SIZE		(512 * 1024 * 1024)
-#endif
 #define CONFIG_DDR_INIT_DELAY		1000
 
-/* Enable passing of ATAGs */
-#define CONFIG_CMDLINE_TAG
-
 /* SMP Spin Table Definitions */
-#define CPU_RELEASE_ADDR                (CONFIG_SYS_SDRAM_BASE + 0x7fff0)
+/* #define CPU_RELEASE_ADDR             (CONFIG_SYS_SDRAM_BASE + 0x7fff0) */
 
 /* Generic Timer Definitions */
-#define CONFIG_SYS_GENERIC_TIMER
-#define COUNTER_FREQUENCY               (12000000)     /* 12MHz */
-#define CONFIG_SYS_FSL_ERRATUM_A008585
+/* #define CONFIG_SYS_GENERIC_TIMER */
+#define CONFIG_SYS_PIT_TIMER
 
-/* Size of malloc() pool */
-#ifdef CONFIG_RUN_FROM_IRAM_ONLY
-#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 1 * 1024 * 1024)
-#else
-#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 2 * 1024 * 1024)
-#endif
-#define CONFIG_BOARD_EARLY_INIT_F
+#define LINFLEXUART_BASE		LINFLEXD0_BASE_ADDR
 
-
-#define CONFIG_FSL_LINFLEXUART
-#define LINFLEXUART_BASE			LINFLEXD0_BASE_ADDR
-
-/* Allow to overwrite serial and ethaddr */
-#define CONFIG_ENV_OVERWRITE
-#define CONFIG_SYS_UART_PORT		(1)
-#define CONFIG_BAUDRATE				115200
-
-
-#undef CONFIG_CMD_IMLS
-
-#define CONFIG_MMC
-#define CONFIG_FSL_ESDHC
 #define CONFIG_SYS_FSL_ESDHC_ADDR	0
-#define CONFIG_SYS_FSL_ESDHC_NUM	1
 
-#define CONFIG_SYS_FSL_ERRATUM_ESDHC111
-
-#define CONFIG_CMD_MMC
-#define CONFIG_GENERIC_MMC
 #define CONFIG_CMD_EXT2 /* EXT2 Support */
-#define CONFIG_CMD_FAT  /* FAT support */
-#define CONFIG_DOS_PARTITION
 
-/* Ethernet config */
-
-#define CONFIG_CMD_PING
-#define CONFIG_CMD_DHCP
-#define CONFIG_CMD_MII
-#define CONFIG_CMD_NET
-#define CONFIG_FEC_MXC
-#define CONFIG_MII
-#define IMX_FEC_BASE            ENET_BASE_ADDR
 #define CONFIG_FEC_XCV_TYPE     RMII
 #define CONFIG_FEC_MXC_PHYADDR  1
 #define CONFIG_PHYLIB
 #define CONFIG_PHY_MICREL
 
-/* I2C Configs */
-#define CONFIG_CMD_I2C
-#define CONFIG_HARD_I2C
-#define CONFIG_I2C_MXC
-#define CONFIG_SYS_I2C
-#define CONFIG_SYS_I2C_MXC
-#define CONFIG_SYS_I2C_BASE		I2C0_BASE_ADDR
-#define CONFIG_SYS_I2C_SPEED	100000
-#define CONFIG_SYS_I2C_SLAVE	0x8
+#define CONFIG_LOADADDR		(DDR_BASE_ADDR + 0x2000000)
 
-#if 0 /* Disable until the FLASH will be implemented */
-#define CONFIG_SYS_USE_NAND
-#endif
-
-#ifdef CONFIG_SYS_USE_NAND
-/* Nand Flash Configs */
-#define	CONFIG_CMD_NAND
-#define CONFIG_JFFS2_NAND
-#define MTD_NAND_FSL_NFC_SWECC 1
-#define CONFIG_NAND_FSL_NFC
-#define CONFIG_SYS_NAND_BASE		0x400E0000
-#define CONFIG_SYS_MAX_NAND_DEVICE	1
-#define NAND_MAX_CHIPS			CONFIG_SYS_MAX_NAND_DEVICE
-#define CONFIG_SYS_NAND_SELECT_DEVICE
-#define CONFIG_SYS_64BIT_VSPRINTF  /* needed for nand_util.c */
-#endif
-
-#define CONFIG_BOOTDELAY		-1
-
-#define CONFIG_LOADADDR			(DDR_BASE_ADDR + 0x2000000)
-#define CONFIG_BOOTARGS			"console=ttyLF0 root=/dev/ram rw"
-
-#define CONFIG_CMD_ENV
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"script=boot.scr\0" \
 	"uimage=uImage\0" \
@@ -160,13 +73,13 @@
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
 	"fdt_file=s32v234-evb.dtb\0" \
-	"fdt_addr=0xC2000000\0" \
+	"fdt_addr=" __stringify(FDT_ADDR) "\0" \
 	"kernel_addr=0xC307FFC0\0" \
 	"ramdisk_addr=0xC4000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
 	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0" \
-	"mmcpart=1\0" \
+	"mmcpart=" __stringify(CONFIG_MMC_PART) "\0" \
 	"mmcroot=/dev/mmcblk0p2 rootwait rw\0" \
 	"update_sd_firmware_filename=u-boot.imx\0" \
 	"update_sd_firmware=" \
@@ -232,96 +145,12 @@
 			"bootm; " \
 		"fi;\0"
 
-#define CONFIG_BOOTCOMMAND \
-	   "mmc dev ${mmcdev}; if mmc rescan; then " \
-		   "if run loadbootscript; then " \
-			   "run bootscript; " \
-		   "else " \
-			   "if run loaduimage; then " \
-				   "run mmcboot; " \
-			   "else run netboot; " \
-			   "fi; " \
-		   "fi; " \
-	   "else run netboot; fi"
-
-/* Miscellaneous configurable options */
-#define CONFIG_SYS_LONGHELP		/* undef to save memory */
-#define CONFIG_SYS_HUSH_PARSER		/* use "hush" command parser */
-#define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
-#define CONFIG_SYS_PROMPT		"=> "
-#undef CONFIG_AUTO_COMPLETE
-#define CONFIG_SYS_CBSIZE		256	/* Console I/O Buffer Size */
-#define CONFIG_SYS_PBSIZE		\
-			(CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
-#define CONFIG_SYS_MAXARGS		16	/* max number of command args */
-#define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
-#define CONFIG_CMDLINE_EDITING
-
-#define CONFIG_CMD_MEMTEST
-#define CONFIG_SYS_MEMTEST_START	(DDR_BASE_ADDR)
-#define CONFIG_SYS_MEMTEST_END		(DDR_BASE_ADDR + (DDR_SIZE - 1))
-
-#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
-#define CONFIG_SYS_HZ				1000
-
-#define CONFIG_SYS_TEXT_BASE        0x3E820000 /* SDRAM */
-#define CONFIG_SYS_TEXT_OFFSET      0x00020000
-
-#ifdef CONFIG_RUN_FROM_IRAM_ONLY
-#define CONFIG_SYS_MALLOC_BASE		(DDR_BASE_ADDR)
-#endif
-
-/*
- * Stack sizes
- * The stack sizes are set up in start.S using the settings below
- */
-#define CONFIG_STACKSIZE		(128 * 1024)	/* regular stack */
-
-#if 0
-/* Configure PXE */
-#define CONFIG_CMD_PXE
-#define CONFIG_BOOTP_PXE
-#define CONFIG_BOOTP_PXE_CLIENTARCH	0x100
-#endif
-
-
-/* Physical memory map */
-#define CONFIG_NR_DRAM_BANKS	1
-#define PHYS_SDRAM			(DDR_BASE_ADDR)
-#define PHYS_SDRAM_SIZE			(DDR_SIZE)
-
-#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
-#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
-#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
-
 #define CONFIG_SYS_INIT_SP_OFFSET \
 	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
-#define CONFIG_SYS_INIT_SP_ADDR \
-	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
-
-/* FLASH and environment organization */
-#define CONFIG_SYS_NO_FLASH
-
-#define CONFIG_ENV_SIZE			(8 * 1024)
-#define CONFIG_ENV_IS_IN_MMC
-
-#define CONFIG_ENV_OFFSET		(12 * 64 * 1024)
-#define CONFIG_SYS_MMC_ENV_DEV		0
-
-#define CONFIG_OF_LIBFDT
-#define CONFIG_CMD_BOOTZ
-
-#define CONFIG_BOOTP_BOOTFILESIZE
-#define CONFIG_BOOTP_BOOTPATH
-#define CONFIG_BOOTP_GATEWAY
-#define CONFIG_BOOTP_HOSTNAME
 
 /* #define CONFIG_CMD_PCI */
-#ifdef CONFIG_CMD_PCI
-#define CONFIG_PCIE_S32V234
-#define CONFIG_PCI
-#define CONFIG_PCI_PNP
-#define CONFIG_PCI_SCAN_SHOW
-#endif
+
+/* we include this file here because it depends on the above definitions */
+#include <configs/s32v234_common.h>
 
 #endif
