d9e761b5cb21076f48449996acfe4548de153f40
Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>
2016-02-23 15:01:23 +0200

    Remove the preprocessor directive before CONFIG_S32V234_FLASH
    The generic support for FLASH on S32V234-EVB, S32V234-PCIE
    and S32V234-TMDP was added. So, the preprocessor directive and
    comment is not anymore valid for current configuration.
    
    Signed-off-by: Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>

--- include/configs/s32v234_common.h
+++ include/configs/s32v234_common.h
@@ -136,9 +136,7 @@
 #define CONFIG_SYS_I2C_SLAVE	0x8
 #define CONFIG_SYS_SPD_BUS_NUM	0
 
-#if 1 /* Disable until the FLASH will be implemented */
 #define CONFIG_S32V234_FLASH
-#endif
 
 /* memory mapped external flash */
 #define CONFIG_SYS_FSL_FLASH0_BASE      0x20000000
