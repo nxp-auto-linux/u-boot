e0e38d8fe4b958fbe37f806afd5ff3a18a509d42
Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>
2017-05-08 16:51:40 +0100

    s32v234bbmini config: Remove unused macro CONFIG_CMD_PCI
    Issue: ALB-1371
    
    Signed-off-by: Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>

--- include/configs/s32v234bbmini.h
+++ include/configs/s32v234bbmini.h
@@ -17,10 +17,7 @@
 
 #define	FDT_FILE s32v234-bbmini.dtb
 
-#define CONFIG_CMD_PCI
-#ifdef CONFIG_CMD_PCI
 #define CONFIG_PCIE_EP_MODE
-#endif
 
 #define CONFIG_BOARD_EXTRA_ENV_SETTINGS \
 	"setphy=mii write 3 d 2; mii write 3 e 2; mii write 3 d 4002; " \
