5ab0b23f75c0a394e9ece7def87bf51bfc8897ee
Larisa Grigore <Larisa.Grigore@nxp.com>
2017-09-13 18:16:16 +0300

    arch:arm: Rename s32v234 into s32xxxx
    Issue: ALB-1650
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>

--- arch/arm/include/asm/gicsupport.h
+++ arch/arm/include/asm/gicsupport.h
@@ -1,7 +1,7 @@
 /*
- * Copyright 2016, NXP
+ * Copyright 2016-2017, NXP
  *
  */
- /* Functions declared in arch/arm/cpu/armv8/s32v234/gicsupport.c */
+ /* Functions declared in arch/arm/cpu/armv8/s32xxxx/gicsupport.c */
 int gic_irq_status(unsigned int irq);
 int gic_register_handler(int irq, void (*handler)(struct pt_regs *pt_regs, unsigned int esr), int type, const char *name);
