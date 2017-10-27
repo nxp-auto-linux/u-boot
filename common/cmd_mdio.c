4f3ecb9eb9d68860a38ebd9f0147ac32815b0193
phu.luuan <phu.luuan@nxp.com>
2017-10-27 15:51:16 +0700

    mdio: Save previous status for mdio list command
    When mdio list command is called, it doesn't save previous parameter.
    So, when we press enter to repeat command, it doesn't show any thing.
    Remove return instruction when call do_mdio with "list" parameter.
    
    Issue:ALB-1741
    
    Signed-off-by: phu.luuan <phu.luuan@nxp.com>

--- common/cmd_mdio.c
+++ common/cmd_mdio.c
@@ -1,6 +1,7 @@
 /*
  * (C) Copyright 2011 Freescale Semiconductor, Inc
  * Andy Fleming
+ * Copyright 2017 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -255,12 +256,6 @@ static int do_mdio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		break;
 	}
 
-	if (op[0] == 'l') {
-		mdio_list_devices();
-
-		return 0;
-	}
-
 	/* Save the chosen bus */
 	miiphy_set_current_dev(bus->name);
 
@@ -273,6 +268,10 @@ static int do_mdio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	case 'r':
 		mdio_read_ranges(phydev, bus, addrlo, addrhi, devadlo, devadhi,
 				 reglo, reghi, extended);
+		break;
+
+	case 'l':
+		mdio_list_devices();
 		break;
 	}
 
