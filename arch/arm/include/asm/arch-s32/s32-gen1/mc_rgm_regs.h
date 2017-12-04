f87fc52b8b21c41603448b00672daa865ee60241
Larisa Grigore <Larisa.Grigore@nxp.com>
2017-12-04 14:33:18 +0200

    arch: s32gen1: mc_rgm initial definitions
    Issue: ALB-1946
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>

---
+++ arch/arm/include/asm/arch-s32/s32-gen1/mc_rgm_regs.h
@@ -0,0 +1,15 @@
+/*
+ * (C) Copyright 2015-2016 Freescale Semiconductor, Inc.
+ * (C) Copyright 2016-2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ARCH_ARM_MACH_S32GEN1_MCRGM_REGS_H__
+#define __ARCH_ARM_MACH_S32GEN1_MCRGM_REGS_H__
+
+#define RGM_PRST(per)			(MC_RGM_BASE_ADDR + 0x30 + (per) * 0x8)
+#define RGM_PSTAT(per)			(MC_RGM_BASE_ADDR + 0x130 + (per) * 0x8)
+
+#endif /* __ARCH_ARM_MACH_S32GEN1_MCRGM_REGS_H__ */
+
---
+++ arch/arm/include/asm/arch-s32/s32v234/mc_rgm_regs.h
@@ -0,0 +1,43 @@
+/*
+ * (C) Copyright 2015-2016 Freescale Semiconductor, Inc.
+ * (C) Copyright 2016-2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ARCH_ARM_MACH_S32V234_MCRGM_REGS_H__
+#define __ARCH_ARM_MACH_S32V234_MCRGM_REGS_H__
+
+#define MC_RGM_DES			(MC_RGM_BASE_ADDR)
+#define MC_RGM_FES			(MC_RGM_BASE_ADDR + 0x300)
+#define MC_RGM_FERD			(MC_RGM_BASE_ADDR + 0x310)
+#define MC_RGM_FBRE			(MC_RGM_BASE_ADDR + 0x330)
+#define MC_RGM_FESS			(MC_RGM_BASE_ADDR + 0x340)
+#define MC_RGM_DDR_HE			(MC_RGM_BASE_ADDR + 0x350)
+#define MC_RGM_DDR_HS			(MC_RGM_BASE_ADDR + 0x354)
+#define MC_RGM_FRHE			(MC_RGM_BASE_ADDR + 0x358)
+#define MC_RGM_FREC			(MC_RGM_BASE_ADDR + 0x600)
+#define MC_RGM_FRET			(MC_RGM_BASE_ADDR + 0x607)
+#define MC_RGM_DRET			(MC_RGM_BASE_ADDR + 0x60B)
+
+/* function reset sources mask */
+#define F_SWT4				0x8000
+#define F_JTAG				0x400
+#define F_FCCU_SOFT			0x40
+#define F_FCCU_HARD			0x20
+#define F_SOFT_FUNC			0x8
+#define F_ST_DONE			0x4
+#define F_EXT_RST			0x1
+
+/* DDR Handshake timeout value in IRC clocks */
+#define HNDSHK_TO_VAL			160
+
+#define MC_RGM_FES_ANY_FUNC_EVENT	0x846D
+#define MC_RGM_DDR_HE_EN		(0x1)
+#define MC_RGM_DDR_HE_VALUE		\
+	(HNDSHK_TO_VAL << 16) | (MC_RGM_DDR_HE_EN)
+#define MC_RGM_FRHE_ALL_VALUE		0x846D
+#define MC_RGM_DDR_HS_HNDSHK_DONE	0x2
+
+#endif /* __ARCH_ARM_MACH_S32V234_MCRGM_REGS_H__ */
+
