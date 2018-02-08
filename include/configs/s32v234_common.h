4c5c7d2778fa3c974445368b22efaa68a4cb55ce
Constantin-dan Avram-nxf32119 <constantin-dan.avram@nxp.com>
2018-02-08 11:21:26 +0000

    configs: define standalone load addr for s32v234
    Define CONFIG_STANDALONE_LOAD_ADDR is not defined for s32v234 platform. It used a
    generic address (defined in arch/arm/config.mk) - 0xc100000 which is not compatible
    with the platform. Redefined to (DDR_BASE_ADDRESS + 0x100000).
    CONFIG_STANDALONE_LOAD_ADDR is the address where standalone application
    should be loaded and run in u-boot environment and is needed at the link
    stage of the application (see example in /examples/standalone/Makefile)
    
    Issue: ALB-2152
    Signed-off-by: Constantin-dan Avram-nxf32119 <constantin-dan.avram@nxp.com>

--- include/configs/s32v234_common.h
+++ include/configs/s32v234_common.h
@@ -1,6 +1,6 @@
 /*
  * (C) Copyright 2015-2016 Freescale Semiconductor, Inc.
- * (C) Copyright 2016-2017 NXP
+ * (C) Copyright 2016-2018 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -43,9 +43,11 @@
 
 /* Run by default from DDR1 */
 #ifdef CONFIG_RUN_FROM_DDR0
-#define DDR_BASE_ADDR		0x80000000
+#define DDR_BASE_ADDR			0x80000000
+#define CONFIG_STANDALONE_LOAD_ADDR	0x80100000
 #else
-#define DDR_BASE_ADDR		0xC0000000
+#define DDR_BASE_ADDR			0xC0000000
+#define CONFIG_STANDALONE_LOAD_ADDR	0xC0100000
 #endif
 
 #define CONFIG_SYS_TEXT_BASE        0x3E820000 /* SDRAM */
