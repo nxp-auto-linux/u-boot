a5e1b76f83771b6474e7a9c128ddac446642a498
Matthew Nunez <matthew.nunez@nxp.com>
2018-04-23 15:17:06 +0700

    mpxs32v234: Add CONFIG_DDR_INIT_DELAY to config header file
    Add CONFIG_DDR_INIT_DELAY 100 (us) to configs/mpxs32v234.h to give the
    second DDR bank enough time to stablize after DDR configuration. This
    will allow the get_ram_size function to detect the full 2 GiB of RAM.
    
    Issue: ALB-2487
    
    Signed-off-by: Matthew Nunez <matthew.nunez@nxp.com>

--- include/configs/mpxs32v234.h
+++ include/configs/mpxs32v234.h
@@ -1,6 +1,7 @@
 /*
  * (C) Copyright 2015-2016 Freescale Semiconductor, Inc.
  * (C) Copyright 2017 MicroSys Electronics GmbH
+ * Copyright 2018 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -16,6 +17,8 @@
 * the ones in the header included at the end, configs/s32v234_common.h */
 
 #define CONFIG_MPXS32V234_R2
+
+#define CONFIG_DDR_INIT_DELAY 100
 
 #define CONFIG_FSL_USDHC
 #define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC_BASE_ADDR
