21d6118a6d143eeb966e0636e03cf29cef741204
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2019-07-10 13:50:22 +0300

    s32-gen1: Rename RUN_AT_EL<x> Kconfig options
    Rename RUN_AT_EL<x> into S32_RUN_AT_EL<x> to better reflect the platform
    dependency.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    Issue: ALB-3880

--- arch/arm/cpu/armv8/s32/fdt.c
+++ arch/arm/cpu/armv8/s32/fdt.c
@@ -16,7 +16,7 @@
 
 #ifdef CONFIG_MP
 
-#if CONFIG_RUN_AT_EL2
+#if CONFIG_S32_RUN_AT_EL2
 /* U-Boot at EL2, presumably because of a Trusted Firmware running at EL3 */
 static void ft_fixup_enable_method(void *blob, int off, u64 __always_unused reg)
 {
@@ -69,7 +69,7 @@ static void ft_fixup_enable_method(void *blob, int off, u64 reg)
 }
 #endif
 
-#if CONFIG_RUN_AT_EL2
+#if CONFIG_S32_RUN_AT_EL2
 /* Add a "psci" node at the top-level of the devide-tree,
  * if it does not already exist
  */
@@ -141,7 +141,7 @@ void ft_fixup_cpu(void *blob)
 						    "cpu", 4);
 	}
 
-#if CONFIG_RUN_AT_EL2
+#if CONFIG_S32_RUN_AT_EL2
 	/* Check if a "psci" node should be added */
 	ft_fixup_psci_node(blob);
 #endif
