1b50912e9f99f7d2ae7e4ae7a93cebd6703464e3
Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>
2020-09-10 18:54:53 +0300

    Extend PRAM to cover optee_os.
    PRAM is extended covering the memory space occupied by optee_os.
    
    Issue: ALB-5680
    Signed-off-by: Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>

--- include/configs/s32g274a.h
+++ include/configs/s32g274a.h
@@ -15,7 +15,12 @@
 #include <configs/s32-gen1.h>
 
 #if !defined(CONFIG_PRAM) && !defined(CONFIG_S32_SKIP_RELOC)
-#define CONFIG_PRAM	2048	/* 2MB */
+
+/* 24 MB covering the following:
+ *  - 22 MB for optee_os + shared memory between optee_os and linux kernel
+ *  - 2 MB for the Secure Monitor
+ */
+#define CONFIG_PRAM	24576	/* 24MB */
 #endif
 
 #if defined(CONFIG_TARGET_S32G274AEVB) && defined(CONFIG_USB)
@@ -24,4 +29,3 @@
 #endif
 
 #endif
-
