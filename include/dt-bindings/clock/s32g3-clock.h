d6e27b11c0cae3ffadda3fcb5060732e045018c7
Larisa Grigore <larisa.grigore@nxp.com>
2021-03-29 16:47:56 +0300

    s32g3: include: Define MC_CGM6_MUXn
    Issue: ALB-6496
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>

---
+++ include/dt-bindings/clock/s32g3-clock.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2021 NXP
+ */
+
+#ifndef __DT_BINDINGS_CLOCK_S32G3_H
+#define __DT_BINDINGS_CLOCK_S32G3_H
+
+#include <dt-bindings/clock/s32g-clock.h>
+
+#define S32G_CLK_MC_CGM6_MUX0		S32G_CLK(18)
+#define S32G_CLK_MC_CGM6_MUX1		S32G_CLK(19)
+#define S32G_CLK_MC_CGM6_MUX2		S32G_CLK(20)
+#define S32G_CLK_MC_CGM6_MUX3		S32G_CLK(21)
+
+#endif //__DT_BINDINGS_CLOCK_S32G3_H
