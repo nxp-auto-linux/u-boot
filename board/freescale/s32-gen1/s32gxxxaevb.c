969c3501e180f85b8ea2677426ec1548e1b3871d
Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
2021-09-21 12:01:08 +0300

    s32g274aevb: probe sja1105 in misc_init_r
    Probe sja1105 in misc_init_r in order to provide clock to the PFE2
    interface.
    
    Issue: ALB-4601
    
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>

--- board/freescale/s32-gen1/s32gxxxaevb.c
+++ board/freescale/s32-gen1/s32gxxxaevb.c
@@ -5,7 +5,10 @@
 #include <asm/arch/soc.h>
 #include <board_common.h>
 #include <common.h>
+#include <dm/uclass.h>
 #include <s32g274a_common.h>
+
+#define SJA1105_NAME	"ethernet-switch@0"
 
 void setup_iomux_uart(void)
 {
@@ -33,3 +36,20 @@ void setup_iomux_uart(void)
 #endif
 }
 
+int misc_init_r(void)
+{
+#if CONFIG_IS_ENABLED(NET) && CONFIG_IS_ENABLED(FSL_PFENG) && \
+	CONFIG_IS_ENABLED(SJA1105)
+	struct udevice *dev;
+
+	/* Probe sja1105 in order to provide a clock for the PFE2 interface,
+	 * otherwise clock init for this interface will fail.
+	 * The return value is not checked on purpose. If the S32G PROCEVB
+	 * board is used without PLATEVB board the uclass_get_device_by_name
+	 * call will fail and returning and error code will break the init_call
+	 * sequence of the u-boot.
+	 */
+	uclass_get_device_by_name(UCLASS_MISC, SJA1105_NAME, &dev);
+#endif
+	return 0;
+}
