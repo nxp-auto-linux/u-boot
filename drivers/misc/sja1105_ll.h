a0607ad14534d38b28c7aa57da1c9cec856b1011
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-01-18 12:06:40 +0200

    misc: sja1105: Make configuration part of the driver
    The selection of SJA1105 switch configuration is attached to the
    match string to avoid code split between driver and board implementation.
    
    Issue: ALB-8329
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
    Signed-off-by: Alexandru-Catalin Ionita <alexandru-catalin.ionita@nxp.com>

--- drivers/misc/sja1105_ll.h
+++ drivers/misc/sja1105_ll.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
 * AVB switch driver module for SJA1105
-* Copyright 2017, 2020, 2023 NXP
+* Copyright 2017, 2020,2022-2023 NXP
 *
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License
@@ -286,7 +286,5 @@ enum sja1105_gen_status_reg {
 };
 
 #define SJA1105_PORT_NB 5
-
-int sja1105_get_cfg(u32 devid, u32 cs, u32 *bin_len, u8 **cfg_bin);
 
 #endif
