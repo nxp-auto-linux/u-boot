2d95f49b5e1284b978435932165f2431f883852e
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-02-07 17:21:58 +0200

    s32-gen1: Remove FEC specific macros
    Issue: ALB-8484
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/configs/s32-gen1.h
+++ include/configs/s32-gen1.h
@@ -40,8 +40,6 @@
 /* we include this file here because it depends on the above definitions */
 #include <configs/s32.h>
 
-#define IMX_FEC_BASE            ENET0_BASE_ADDR
-
 #define CONFIG_SYS_TEXT_OFFSET      0x00020000
 
 #define S32_SRAM_BASE		0x34000000
