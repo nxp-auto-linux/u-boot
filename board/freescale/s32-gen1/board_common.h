ce5ecbcaaf6ccfe1d904f42d199f27ff044e43bc
Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
2022-02-09 10:52:24 +0200

    board: s32-gen1: remove setup_iomux_uart* definitions
    Issue: ALB-8367
    
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>

--- board/freescale/s32-gen1/board_common.h
+++ board/freescale/s32-gen1/board_common.h
@@ -9,15 +9,6 @@
 #include <config.h>
 #include <asm/types.h>
 
-void setup_iomux_uart(void);
-
-void setup_iomux_uart0_pc09_pc10(void);
-
-#if defined(CONFIG_TARGET_S32G274AEMU) || \
-	defined(CONFIG_TARGET_S32G399AEMU)
-void setup_iomux_uart1_pb09_pb10(void);
-#endif
-
 #if CONFIG_IS_ENABLED(NETDEVICES)
 void ft_enet_fixup(void *fdt);
 u32 s32ccgmac_cfg_get_mode(int cardnum);
