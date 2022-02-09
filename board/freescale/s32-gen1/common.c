afa192d97e5082c5365708851d771844120f658e
Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
2022-02-09 11:13:56 +0200

    board: s32-gen1: remove include guarded by CONFIG_TARGET*
    Issue: ALB-8367
    
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>

--- board/freescale/s32-gen1/common.c
+++ board/freescale/s32-gen1/common.c
@@ -12,13 +12,6 @@
 #include <miiphy.h>
 #include <asm/arch/s32-gen1/ncore.h>
 
-#if defined(CONFIG_TARGET_S32G2XXAEVB) || defined(CONFIG_TARGET_S32G3XXAEVB) ||\
-	defined(CONFIG_NXP_S32GRDB_BOARD)
-#include <dm/uclass.h>
-#include <misc.h>
-#include <s32gen1_siul2_nvram.h>
-#endif
-
 #include "board_common.h"
 
 DECLARE_GLOBAL_DATA_PTR;
