a8037649544af0a435c89d1f8caf6efa5758a371
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-03-09 12:57:29 +0200

    arch: Move s32 folder under mach-s32
    Issue: ALB-8576
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- arch/arm/include/asm/gicsupport.h
+++ arch/arm/include/asm/gicsupport.h
@@ -1,8 +1,8 @@
 /*
- * Copyright 2016-2018, NXP
+ * Copyright 2016-2018, 2022 NXP
  *
  */
- /* Functions declared in arch/arm/cpu/armv8/s32/gicsupport.c */
+ /* Functions declared in arch/arm/mach-s32/s32-cc/gicsupport.c */
 int gic_irq_status(unsigned int irq);
 int gic_register_handler(int irq,
 			 void (*handler)(struct pt_regs *pt_regs,
