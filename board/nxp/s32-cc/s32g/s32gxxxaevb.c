886ea2297be5069b069a0e226f55c5a8023d9ff0
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-03-10 16:33:55 +0200

    arch: Make SAF1508 initialization part of S32GEVB init
    Issue: ALB-8576
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- board/nxp/s32-cc/s32g/s32gxxxaevb.c
+++ board/nxp/s32-cc/s32g/s32gxxxaevb.c
@@ -5,8 +5,53 @@
 #include <board_common.h>
 #include <common.h>
 #include <dm/uclass.h>
+#include <generic-phy.h>
+#include <dm/device.h>
+#include <dm/device-internal.h>
 
 #define SJA1105_NAME	"ethernet-switch@0"
+
+static int enable_saf1508bet(void)
+{
+	int ret = 0;
+	struct udevice *dev;
+	struct phy phy;
+	struct uclass *uc;
+	struct udevice *bus;
+
+	ret = uclass_get_device(UCLASS_USB, 0, &dev);
+	if (ret) {
+		pr_err("%s: Cannot find USB device\n", __func__);
+		return ret;
+	}
+	ret = uclass_get(UCLASS_USB, &uc);
+	if (ret)
+		return ret;
+
+	/* Probe USB controller */
+	uclass_foreach_dev(bus, uc) {
+		ret = device_probe(bus);
+		if (ret == -ENODEV) {	/* No such device. */
+			puts("Port not available.\n");
+			continue;
+		}
+	}
+
+	/* SAF1508BET PHY */
+	ret = generic_phy_get_by_index(dev, 0, &phy);
+	if (ret) {
+		pr_err("failed to get %s USB PHY\n", dev->name);
+		return ret;
+	}
+
+	ret = generic_phy_power_on(&phy);
+	if (ret) {
+		pr_err("failed to get %s USB PHY\n", dev->name);
+		return ret;
+	}
+
+	return ret;
+}
 
 int misc_init_r(void)
 {
@@ -23,5 +68,11 @@ int misc_init_r(void)
 	 */
 	uclass_get_device_by_name(UCLASS_MISC, SJA1105_NAME, &dev);
 #endif
+	/* The usb phy must be probed in u-boot in order to have a working USB
+	 * interface in linux.
+	 */
+	if (IS_ENABLED(CONFIG_SAF1508BET_USB_PHY))
+		enable_saf1508bet();
+
 	return 0;
 }
