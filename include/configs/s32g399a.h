64dba55f89f9d39c363af52da46cdacce7f53af3
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-04-01 16:14:18 +0300

    board: nxp: Add support for S32G399AEVB
    S32G3 is a family of high-performance vehicle network processors
    that combine controller area network (CAN), local interconnect
    network (LIN), and FlexRay networking with high data rate
    Ethernet networking. The S32G3 chips include a functional safety
    infrastructure and a hardware security engine. The S32G3 chips are
    pin-for-pin compatible with S32G2 chips, providing over 2x
    performance and system RAM.
    
    Issue: ALB-8614
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/configs/s32g399a.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright 2021-2022 NXP
+ */
+
+/*
+ * Configuration settings for all the Freescale S32G399A boards.
+ */
+
+#ifndef __S32G399A_H__
+#define __S32G399A_H__
+
+#include <configs/s32-cc.h>
+
+#endif
