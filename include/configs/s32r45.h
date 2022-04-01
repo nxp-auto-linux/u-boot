d63a2d65bb420d129245b53b6c5a56da4d589de1
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-04-01 17:25:47 +0300

    board: nxp: Add support for S32R45EVB
    Add support for the NXP S32R45EVB board.
    S32R45 is a RADAR application microprocessor unit (MPU) based on
    ARM® Cortex®-A53 and ARM® Cortex®-M7 cores. It extends
    the existing S32R product family, which includes the
    MPC5775K, S32R294, S32R274, and S32R372 products.
    The S32R45 design targets the civil automotive ADAS RADAR market
    as its primary application. It can as well be used for a variety of
    applications in the industrial and consumer markets.
    
    Issue: ALB-8616
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/configs/s32r45.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2017-2022 NXP
+ */
+
+#ifndef __S32R45_H__
+#define __S32R45_H__
+
+#include <configs/s32-cc.h>
+
+#endif
---
+++ include/configs/s32r45evb.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef __S32R45EVB_H__
+#define __S32R45EVB_H__
+
+#include <configs/s32r45.h>
+
+#endif
