efa0497baecd8746f0c44dae9c9eaaf52f6c3ec9
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-04-07 16:36:59 +0300

    board: nxp: Add minimal support for S32G274ABLUEBOX3
    S32G274ABLUEBOX platform is a dual SoC board with
    S32G2 and LX2, where these two SoCs are connected
    together via PCIe.
    Also, this board has rich networking features, such as:
    PFE, DPAA2 and three sja1110 switches.
    
    Issue: ALB-8617
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/configs/s32g274abluebox3.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright 2022 NXP
+ */
+#ifndef __S32G274ABLUEBOX3_H__
+#define __S32G274ABLUEBOX3_H__
+
+#define CONFIG_EXTRA_ENV_SETTINGS   S32CC_ENV_SETTINGS
+#define EXTRA_BOOT_ARGS         ""
+
+#define FDT_FILE			"s32g274a-bluebox3.dtb"
+
+#include <configs/s32g2.h>
+
+#endif
