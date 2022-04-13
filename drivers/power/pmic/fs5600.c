3f2297931585a3293bd47453369cd1c575495cd9
Andrei Botila <andrei.botila@nxp.com>
2022-04-13 17:13:21 +0300

    pmic: fs5600: add nxp name and relocate config
    Issue: ALB-8595
    Signed-off-by: Andrei Botila <andrei.botila@nxp.com>

--- drivers/power/pmic/fs5600.c
+++ drivers/power/pmic/fs5600.c
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause
 /*
- * Copyright 2020 NXP
+ * Copyright 2020, 2022 NXP
  */
 
 #include <common.h>
-#include <errno.h>
 #include <dm.h>
+#include <errno.h>
 #include <i2c.h>
 #include <power/pmic.h>
 
