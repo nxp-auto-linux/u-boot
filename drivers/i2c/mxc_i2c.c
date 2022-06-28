ec001280ef3eb773356786c5215bbb339a303999
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-06-28 14:09:16 +0300

    i2c: mxc: Replace fdt calls with dev equivalents
    Issue: ALB-9010
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- drivers/i2c/mxc_i2c.c
+++ drivers/i2c/mxc_i2c.c
@@ -27,9 +27,6 @@
 #include <watchdog.h>
 #include <dm.h>
 #include <dm/pinctrl.h>
-#include <fdtdec.h>
-
-DECLARE_GLOBAL_DATA_PTR;
 
 #define I2C_QUIRK_FLAG		(1 << 0)
 
@@ -890,14 +887,12 @@ static int mxc_i2c_set_bus_speed(struct udevice *bus, unsigned int speed)
 static int mxc_i2c_probe(struct udevice *bus)
 {
 	struct mxc_i2c_bus *i2c_bus = dev_get_priv(bus);
-	const void *fdt = gd->fdt_blob;
-	int node = dev_of_offset(bus);
 	fdt_addr_t addr;
 	int ret, ret2;
 
 	i2c_bus->driver_data = dev_get_driver_data(bus);
 
-	addr = devfdt_get_addr(bus);
+	addr = dev_read_addr(bus);
 	if (addr == FDT_ADDR_T_NONE)
 		return -EINVAL;
 
@@ -927,16 +922,18 @@ static int mxc_i2c_probe(struct udevice *bus)
 	 * See Documentation/devicetree/bindings/i2c/i2c-imx.txt
 	 * Use gpio to force bus idle when necessary.
 	 */
-	ret = fdt_stringlist_search(fdt, node, "pinctrl-names", "gpio");
+	ret = dev_read_stringlist_search(bus, "pinctrl-names", "gpio");
 	if (ret < 0) {
 		debug("i2c bus %d at 0x%2lx, no gpio pinctrl state.\n", bus->seq, i2c_bus->base);
 	} else {
-		ret = gpio_request_by_name_nodev(offset_to_ofnode(node),
-				"scl-gpios", 0, &i2c_bus->scl_gpio,
-				GPIOD_REQUESTED);
-		ret2 = gpio_request_by_name_nodev(offset_to_ofnode(node),
-				"sda-gpios", 0, &i2c_bus->sda_gpio,
-				GPIOD_REQUESTED);
+		ret = gpio_request_by_name_nodev(dev_ofnode(bus),
+						 "scl-gpios", 0,
+						 &i2c_bus->scl_gpio,
+						 GPIOD_REQUESTED);
+		ret2 = gpio_request_by_name_nodev(dev_ofnode(bus),
+						  "sda-gpios", 0,
+						  &i2c_bus->sda_gpio,
+						  GPIOD_REQUESTED);
 		if (!dm_gpio_is_valid(&i2c_bus->sda_gpio) ||
 		    !dm_gpio_is_valid(&i2c_bus->scl_gpio) ||
 		    ret || ret2) {
