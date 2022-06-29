b757953926e533847594472957ef43e6af202626
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-06-29 11:19:06 +0300

    qspi: env: Remove obsolete flash environment offsets
    These are not needed when using mtd qspi partitions approach.
    
    Issue: ALB-9013
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

--- include/configs/s32-cc.h
+++ include/configs/s32-cc.h
@@ -52,42 +52,6 @@
 #define CONFIG_SYS_MMC_ENV_DEV		0
 #define MMC_PART_FAT			1
 
-/**
- * Note: The *_FLASH_ADDR and *_FLASH_MAXSIZE macros are used
- * with the 'setexpr' command. Therefore ensure none of them expand
- * into operations with more than two operands and avoid unnecessary
- * parantheses. Also these should be kept in sync with
- * 'conf/machine/include/s32*flashmap.inc'.
- *
- * QSPI flash map:
- *
- * Name		Size			Offset
- * FIP		~1.9M			0x0
- * QSPI env	64K(CONFIG_ENV_SIZE)	0x01e0000(CONFIG_ENV_OFFSET)
- * Image	14M			0x0e00000
- * Linux DTB	1M			0x0ff0000
- * Ramdisk	32M			0x10f0000
- */
-#define KERNEL_FLASH_MAXSIZE		0x0e00000
-#define FDT_FLASH_MAXSIZE		0x0100000
-#define RAMDISK_FLASH_MAXSIZE		0x2000000
-#define FIP_FLASH_ADDR			0x0000000
-#define KERNEL_FLASH_ADDR		0x01f0000
-#define FDT_FLASH_ADDR			0x0ff0000
-#define RAMDISK_FLASH_ADDR		0x10f0000
-
-#if defined(CONFIG_ENV_IS_IN_SPI_FLASH)
-#  if (CONFIG_ENV_OFFSET + CONFIG_ENV_SIZE > KERNEL_FLASH_ADDR)
-#    error "Environment and Kernel would overlap in flash memory"
-#  endif
-#endif
-#if (KERNEL_FLASH_ADDR + KERNEL_FLASH_MAXSIZE > FDT_FLASH_ADDR)
-#  error "Kernel and FDT would overlap in flash memory"
-#endif
-#if (FDT_FLASH_ADDR + FDT_FLASH_MAXSIZE > RAMDISK_FLASH_ADDR)
-#  error "FDT and Ramdisk would overlap in flash memory"
-#endif
-
 #define S32CC_ENV_SETTINGS \
 	BOOTENV \
 	"boot_mtd=booti\0" \
@@ -104,16 +68,7 @@
 		"${boot_mtd} ${loadaddr} ${ramdisk_addr} ${fdt_addr};\0" \
 	"flashbootargs=setenv bootargs console=${console},${baudrate}" \
 		" root=/dev/ram rw earlycon " EXTRA_BOOT_ARGS ";"\
-		"setenv fip_flashaddr " __stringify(FIP_FLASH_ADDR) ";" \
-		"setenv kernel_flashaddr " __stringify(KERNEL_FLASH_ADDR) ";" \
-		"setenv flashsize " __stringify(FSL_QSPI_FLASH_SIZE) ";" \
-		"setenv kernel_maxsize " __stringify(KERNEL_FLASH_MAXSIZE) ";" \
-		"setenv fdt_flashaddr " __stringify(FDT_FLASH_ADDR) ";" \
-		"setenv fdt_maxsize " __stringify(FDT_FLASH_MAXSIZE) ";" \
-		"setenv ramdisk_flashaddr " \
-				__stringify(RAMDISK_FLASH_ADDR) ";" \
-		"setenv ramdisk_maxsize " \
-				__stringify(RAMDISK_FLASH_MAXSIZE) ";\0" \
+		"setenv flashsize " __stringify(FSL_QSPI_FLASH_SIZE) ";\0" \
 	"image=Image\0" \
 	"initrd_high=" __stringify(S32CC_INITRD_HIGH_ADDR) "\0" \
 	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}; " \
