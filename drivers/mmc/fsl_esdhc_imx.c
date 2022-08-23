991ec1f13b2c2e30919a81cf28f2d60c102f941d
Dan Nica <dan.nica@nxp.com>
2022-08-23 10:39:45 +0300

    s32cc: mmc: Only check for IRQSTAT_BRR in case of Standard Tuning
    In case of the Standard Tuning Procedure IRQSTAT only sets the BRR bit and
    nothing else. But this does not apply for the Manual Tuning Procedure.
    
    Issue: ALB-8733
    Signed-off-by: Dan Nica <dan.nica@nxp.com>

--- drivers/mmc/fsl_esdhc_imx.c
+++ drivers/mmc/fsl_esdhc_imx.c
@@ -484,8 +484,9 @@ static int esdhc_send_cmd_common(struct fsl_esdhc_priv *priv, struct mmc *mmc,
 	esdhc_write32(&regs->xfertyp, xfertyp);
 #endif
 
-	if ((cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK) ||
-	    (cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK_HS200))
+	if (priv->flags & ESDHC_FLAG_STD_TUNING &&
+	    (cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK ||
+	     cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK_HS200))
 		flags = IRQSTAT_BRR;
 
 	/* Wait for the command to complete */
@@ -557,10 +558,10 @@ static int esdhc_send_cmd_common(struct fsl_esdhc_priv *priv, struct mmc *mmc,
 		esdhc_pio_read_write(priv, data);
 #else
 		flags = DATA_COMPLETE;
-		if ((cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK) ||
-		    (cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK_HS200)) {
+		if (priv->flags & ESDHC_FLAG_STD_TUNING &&
+		    (cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK ||
+		     cmd->cmdidx == MMC_CMD_SEND_TUNING_BLOCK_HS200))
 			flags = IRQSTAT_BRR;
-		}
 
 		do {
 			irqstat = esdhc_read32(&regs->irqstat);
