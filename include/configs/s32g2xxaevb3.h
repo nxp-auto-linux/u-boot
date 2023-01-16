ec33c378b8c7bca20e46fb118b202fb1a6a2f0dd
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-01-16 09:09:14 +0200

    Add s32g2xxaevb3 board
    Issue: ALB-9666
    Issue: ALB-9779
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ include/configs/s32g2xxaevb3.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright 2023 NXP
+ */
+#ifndef __S32G2XXAEVB3_H__
+#define __S32G2XXAEVB3_H__
+
+#include <configs/s32g2.h>
+
+#define EXTRA_BOOTCOMMAND		PFE_INIT_CMD
+#define EXTRA_BOOT_ARGS			PFE_EXTRA_BOOT_ARGS
+#define FDT_FILE			"s32g2xxa-evb3.dtb"
+
+#endif
