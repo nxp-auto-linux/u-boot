da6cb2614ff930048032d25a3ac86e2ef10c4315
Alexandru-Catalin Ionita <alexandru-catalin.ionita@nxp.com>
2023-07-25 13:04:59 +0300

    s32r: fix guards typo
    For S32R include files, the guard had a typo :)
    
    Issue: ALB-10118
    Signed-off-by: Alexandru-Catalin Ionita <alexandru-catalin.ionita@nxp.com>

--- include/configs/s32r45evb.h
+++ include/configs/s32r45evb.h
@@ -1,9 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Copyright 2022 NXP
+ * Copyright 2022-2023 NXP
  */
-#ifndef __S32GR45EVB_H__
-#define __S32GR45EVB_H__
+#ifndef __S32R45EVB_H__
+#define __S32R45EVB_H__
 
 #include <configs/s32r45.h>
 
