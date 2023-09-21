d47bfa9670e243a463cd811193b6f300575025fa
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-09-21 16:02:01 +0300

    dt-bindings: s32g-gpr-nvmem: Add PFE_EMACS_INTF_SEL cell
    Issue: ALB-10366
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

---
+++ include/dt-bindings/nvmem/s32g-gpr-nvmem.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef S32G_GPR_NVMEM_H
+#define S32G_GPR_NVMEM_H
+
+#define S32G_GPR_PFE_EMACS_INTF_SEL_OFFSET	0x80
+
+#define S32G_GPR_CELL_SIZE			0x4
+
+#endif
