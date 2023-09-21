96097391cf110af4f61a2792643d1e7e92a73c06
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-09-21 14:43:49 +0300

    dt-bindings: nvmem: Add `s32cc-gpr-nvmem` offsets
    Issue: ALB-10366
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

---
+++ include/dt-bindings/nvmem/s32cc-gpr-nvmem.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef S32CC_GPR_NVMEM_H
+#define S32CC_GPR_NVMEM_H
+
+#define S32CC_GPR_LOCKSTEP_ENABLED_OFFSET	0x0
+
+#define S32CC_GPR_CELL_SIZE			0x4
+
+#endif
