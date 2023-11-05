a98e96eb4856d491dc26ccda48f5b1096212550f
Martin Hrdlicka <martin.hrdlicka@nxp.com>
2023-11-05 19:15:31 +0100

    dt-bindings: s32g-gpr-nvmem: Add PFE_GENCTRL3 cell
    Issue: ALB-10445
    Signed-off-by: Martin Hrdlicka <martin.hrdlicka@nxp.com>

--- include/dt-bindings/nvmem/s32g-gpr-nvmem.h
+++ include/dt-bindings/nvmem/s32g-gpr-nvmem.h
@@ -10,6 +10,7 @@
 #define S32G_GPR_PFE_COH_EN_OFFSET              0x81
 #define S32G_GPR_PFE_PWR_CTRL_OFFSET            0x82
 #define S32G_GPR_PFE_EMACS_GENCTRL1_OFFSET      0x83
+#define S32G_GPR_PFE_GENCTRL3_OFFSET            0x84
 
 #define S32G_GPR_CELL_SIZE			0x4
 
